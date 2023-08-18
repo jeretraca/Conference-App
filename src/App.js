4011d7f70e347c2c6c74772ca54553db4f109e4a
jeretraca <jeretraca@gmail.com>
2023-08-18 22:48:30 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import Home from './Home.js';
+import Speakers from './Speakers.js';
+
+export const ConfigContext = createContext();
+
+const pageToShow = (pageName) => {
+  if(pageName === "Home") return <Home />;
+  if(pageName === "Speakers") return <Speakers />;
+    return <div>Not Found</div>;
+};
+
+const configValue = {
+    showSignMeUp: true,
+    showSpeakerSpeakingDays: false
+};
 
 const App = ({ pageName }) => {
-  if (pageName === 'Home') return <Home></Home>;
-  if (pageName === 'Speakers') return <></>;
-  return <div>Not Found</div>;
+  return (
+    <ConfigContext.Provider value={configValue}>
+      <div>
+      {pageToShow(pageName)}
+      </div>
+    </ConfigContext.Provider>
+  );
 };
 
 export default App;
---
+++ src/SpeakersReducer.js
@@ -0,0 +1,25 @@
+const SpeakersReducer = (state, action) => {
+    function updateFavorite(favoriteValue) {
+        return state.map((item, index) => {
+          if (item.id === action.sessionId) {
+            return { ...item, favorite: favoriteValue };
+          }
+          return item;
+        });
+      }
+      switch (action.type) {
+        case 'setSpeakerList': {
+          return action.data;
+        }
+        case 'favorite': {
+          return updateFavorite(true);
+        }
+        case 'unfavorite': {
+          return updateFavorite(false);
+        }
+        default:
+          return state;
+      }
+};
+ 
+export default SpeakersReducer;
